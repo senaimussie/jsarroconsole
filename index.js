c1a5a2adfe9377bd6d2bf336e3fdcc2d3957ab21
senaimussie <128145833+senaimussie@users.noreply.github.com>
2024-10-23 12:18:53 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let sum = (a,b) => a +b;{
+c = sum(45,78)};
+console.log(c);
+
+let m = (e,f) => e * f;{
+ d = m(45,6) };
+console.log(d);
+
+let greet = (name) => "hello " + name;{  
+    gre = greet("Senai")};
+    console.log(gre);
